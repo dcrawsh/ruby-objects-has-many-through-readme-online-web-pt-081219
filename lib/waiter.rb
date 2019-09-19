eca7565592c41b34a3ee85b632e874e31eaca011
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-19 20:08:22 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,8 +1,13 @@
 class Waiter
-
+@@all = Array.new 
 def initialize(name,experience)
   @name = name 
   @experience = experience 
+  Waiter.all << self 
+end 
+
+def self.all 
+  @@all
 end 
 
 end
